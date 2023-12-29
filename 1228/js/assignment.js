79de97c8c8a50417866b2233dcc50e0e5735ff6d
JYH <jyh010624@gamil.com>
2023-12-29 09:37:29 +0900

    assignment

--- 1228/js/assignment.js
+++ 1228/js/assignment.js
@@ -25,6 +25,8 @@
   console.log(result)
 } */
 
+
+/*
 function maxdiv() {
   var x = parseInt(prompt("첫 번째 숫자를 입력하세요."));
   var y = parseInt(prompt("두 번째 숫자를 입력하세요."));
@@ -44,18 +46,46 @@ function maxdiv() {
   }
   document.write("두 수의 최대공약수는 "+findDiv+"입니다.");
 }
+*/
 
+// ************************************************************************************
 
+// 1. 최대공약수
+function func_2 () {
+function getGCD(n,m) {
+  let max = n > m ? n : m //삼중조건문 (조건식?True:False)
+  let GCD = 0;
 
-function isPositive() {
-  let number = parseInt(prompt("숫자를 입력하세요."))
-  if (number > 0) {
-    alert('양수입니다.')
-  } else if (number < 0) {alert('음수입니다.')}
-    else if (number == 0) {alert('0입니다.')}
+  for (let i = 1 ; i<= max ; i++) {
+    if (n%i == 0 && m%i == 0) {
+      GCD = i;
+    }
+  } 
+  return GCD
+}
+let num1 = parseInt(prompt("첫 번째 숫자를 입력하세요."))
+let num2 = parseInt(prompt("두 번째 숫자를 입력하세요."))
+let result = getGCD(num1, num2)
+document.write("두 수의 최대공약수는 "+result+"입니다.")
 }
 
 
+// 2. 양수 음수
+function func_1() {
+function isPositive(n) {
+  
+  if (n > 0) {
+    alert('양수입니다.')
+  } else if (n < 0) {alert('음수입니다.')}
+    else if (n == 0) {alert('0입니다.')}
+}
+
+let number = parseInt(prompt("숫자를 입력하세요."))
+if (!isNaN(number)) {    //숫자일 경우에만 실행 = number != NaN
+  isPositive(number);
+}
+
+}
 
 
 
